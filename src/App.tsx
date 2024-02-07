0199537c05ec0a339601f185584d9d6fb79fe780
Honey Patel <honeydpatel2523@gmail.com>
2024-02-07 17:52:09 +0530

    feat: add error for browsers which not support push notifications

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home"
 import OneSignal from 'react-onesignal';
 import { useContext, useEffect } from "react";
 import { ReminderContext } from "./context/reminder/ReminderContext";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
@@ -13,9 +13,17 @@ function App() {
   useEffect(() => {
     async function runOnesignal() {
       await OneSignal.init({ appId: import.meta.env.VITE_ONESIGNAL_APP_ID, allowLocalhostAsSecureOrigin: true });
-      getUser();
-      OneSignal.User.PushSubscription.addEventListener("change", getUser);
+
+      if (OneSignal.Notifications.isPushSupported()) {
+        getUser();
+        OneSignal.User.PushSubscription.addEventListener("change", getUser);
+        OneSignal.Slidedown.promptPush()
+        return;
+      }
+
+      toast.error("Your Browser doesn't support push notifications, Try another browser !")
     }
+
     runOnesignal();
     return () => {
       OneSignal.User.PushSubscription.removeEventListener("change", getUser);
